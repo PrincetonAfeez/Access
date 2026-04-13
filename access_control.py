6193a8231472e6e61aa39cfebdcc0a2c594c8def
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-13 11:28:49 -0700

    Facility access control domain model for Vault OS

---
+++ access_control.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from collections import defaultdict, deque
+from dataclasses import dataclass
+from datetime import date, datetime, time, timedelta
+from enum import IntEnum
+from itertools import count
+
