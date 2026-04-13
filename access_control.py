fe060493b844f6ba74377bf136ff2a4366213f5b
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-13 11:29:41 -0700

    Return *moment* as a naive datetime in facility-local wall-clock semantics

--- access_control.py
+++ access_control.py
@@ -6,3 +6,10 @@ from datetime import date, datetime, time, timedelta
 from enum import IntEnum
 from itertools import count
 
+def naive_facility_moment(moment: datetime) -> datetime:
+
+    if moment.tzinfo is None:
+        return moment
+    return moment.astimezone().replace(tzinfo=None)
+
+
